292e5b903ff453d1d1de3b4413b42cad308f3ede
Kristof Neirynck <k.neirynck@gmail.com>
2012-06-03 15:38:49 +0200

    FolderListener now listens to subfolders

--- src/whenchanged/FolderListener.java
+++ src/whenchanged/FolderListener.java
@@ -2,8 +2,12 @@ package whenchanged;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
@@ -11,59 +15,197 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+/*
+ * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+/**
+ * Example to watch a directory (or tree) for changes to files.
+ * @see http://docs.oracle.com/javase/tutorial/essential/io/examples/WatchDir.java
+ */
 public class FolderListener {
 
-    private static final Logger logger = Logger.getLogger(CommandRunnable.class.getName());
+    private static final Logger logger = Logger.getLogger(FolderListener.class.getName());
+    
     private final Runnable callback;
     private final List<String> folders;
-            
-    public FolderListener(Runnable callback, final List<String> folders) {
-        this.callback = callback;
-        this.folders = folders;
+    
+    private final WatchService watcher;
+    private final Map<WatchKey,Path> keys;
+    private final boolean recursive;
+    private final boolean trace;
+
+    @SuppressWarnings("unchecked")
+    private static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
     }
 
-    public void run() {
+    /**
+     * Register the given directory with the WatchService
+     */
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        if (trace) {
+            Path prev = keys.get(key);
+            if (prev == null) {
+                logger.fine(String.format("register: %s", dir));
+            } else {
+                if (!dir.equals(prev)) {
+                    logger.fine(String.format("update: %s -> %s", prev, dir));
+                }
+            }
+        }
+        keys.put(key, dir);
+    }
 
-        try {
-            WatchService watcher = FileSystems.getDefault().newWatchService();
-            for (String folder : folders) {
-                logger.fine(String.format("register watcher for folder: %s", folder));
-                Path path = Paths.get(folder).toAbsolutePath();
-                if (path.toFile().isDirectory()) {
-                    path.register(watcher,
-                            ENTRY_CREATE,
-                            ENTRY_DELETE,
-                            ENTRY_MODIFY);
-                } else {
-                    logger.warning(String.format("not a directory: %s", path.toString()));
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException
+            {
+                register(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * Process all events for keys queued to the watcher
+     */
+    private void processEvents() {
+        for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+
+            for (WatchEvent<?> event: key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                // TBD - provide example of how OVERFLOW event is handled
+                if (kind == OVERFLOW) {
+                    continue;
+                }
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                // print out event
+                logger.info(String.format("%s: %s", event.kind().name(), child));
+                
+                // if directory is created, and watching recursively, then
+                // register it and its sub-directories
+                if (recursive && (kind == ENTRY_CREATE)) {
+                    try {
+                        if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
+                            registerAll(child);
+                        }
+                    } catch (IOException x) {
+                        // ignore to keep sample readbale
+                    }
                 }
             }
 
-            for (;;) {
-                WatchKey key = watcher.take();
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-                    if (kind == OVERFLOW) {
-                        continue;
-                    }
-                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path filename = ev.context();
-                    //Path child = dir.resolve(filename);
-                    logger.fine(String.format("change detected: %s %s", filename.toString(), kind));
-                    callback.run();
-                }
-                boolean valid = key.reset();
-                if (!valid) {
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
                     break;
                 }
             }
-        } catch (IOException | InterruptedException ex) {
-            logger.log(Level.SEVERE, null, ex);
+            
+            // hook method
+            callback.run();
         }
-
     }
+    
+    public FolderListener(Runnable callback, final List<String> folders) throws IOException {
+        this.trace = true;
+        this.recursive = true;
+        this.callback = callback;
+        this.folders = folders;
+        this.keys = new HashMap<>();
+        this.watcher = FileSystems.getDefault().newWatchService();
+    }
+    
+    public void run(){
+        try {
+            for (String folder : folders) {
+                Path path = Paths.get(folder).toAbsolutePath();
+                if (path.toFile().isDirectory()) {
+                    if (recursive) {
+                        logger.fine(String.format("Scanning %s ...", path));
+                        registerAll(path);
+                        logger.fine("Done.");
+                    } else {
+                        register(path);
+                    }
+                } else {
+                    logger.warning(String.format("%s is not a directory", folder));
+                }
+            }
+        } catch (IOException ex) {
+            logger.warning("oops");
+        }
+        this.processEvents();
+        
+    }
+    
 }
--- src/whenchanged/Whenchanged.java
+++ src/whenchanged/Whenchanged.java
@@ -1,5 +1,7 @@
 package whenchanged;
 
+import java.io.IOException;
+
 public class Whenchanged {
 
     private static String PROPERTIES_FILENAME = "whenchanged.properties";
@@ -12,6 +14,11 @@ public class Whenchanged {
         Settings settings = new Settings(propertiesFilename).load();
         Runnable callback = new CommandRunnable(settings.getCommandDir(), settings.getCommand());
         Runnable debounced = new DebouncedRunnable(callback, settings.getDelay());
-        new FolderListener(debounced, settings.getFolders()).run();
+        try {
+            new FolderListener(debounced, settings.getFolders()).run();
+        } catch (IOException ex) {
+            System.err.println("something went wrong");
+            System.exit(1);
+        }
     }
 }
